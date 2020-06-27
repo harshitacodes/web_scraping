0814be1a82c05070138576a84565b0e48b3ae5e0
harshita <harshitacodes>
2020-06-27 21:39:22 +0530

    adding files

---
+++ imdb_group_movies.py
@@ -0,0 +1,77 @@
+import requests
+import pprint
+from bs4 import BeautifulSoup
+
+URL = " https://www.imdb.com/india/top-rated-indian-movies/" #url of the imdb.com 
+sample = requests.get(URL)
+
+soup = BeautifulSoup(sample.text,"html.parser")
+
+
+def scrape_top_list():
+    m_div = soup.find('div',class_ = 'lister')
+    t_body = m_div.find('tbody',class_ = 'lister-list')
+    tr_s = t_body.find_all('tr')
+
+    movies_name = []
+    movies_ranks = []
+    release_year = []
+    movies_url = []
+    movies_ranting = []
+
+    for tr in tr_s:
+        position = tr.find('td', class_ ="titleColumn").get_text().strip()
+        rank = ''
+        for i in position:
+            if '.' not in i:
+                rank = rank + i
+            else:
+                break
+        movies_ranks.append(rank)
+
+        title = tr.find('td',class_ ="titleColumn").a.get_text()
+        movies_name.append(title)
+
+        years = tr.find('td', class_ ="titleColumn").span.get_text()
+        release_year.append(years)
+
+        ratings = tr.find('td', class_ = "ratingColumn").strong.get_text()
+        movies_ranting.append(ratings)
+
+        links = tr.find('td', class_ = "titleColumn").a['href']
+        movies_link = "https://www.imdb.com" + links
+        movies_url.append(movies_link)
+        
+
+        All_movies = []
+        details = {'position':'','name':'','year':'','rating':'','url':''}
+        for i in range(0,len(movies_ranks)):
+            details['position'] = int(movies_ranks[i])
+            details['name'] = str(movies_name[i])
+            year_of_release = release_year[i][1:5]
+            details['year'] = int(year_of_release)
+            details['rating'] = float(movies_ranting[i])
+            details['url'] = movies_url[i]
+            All_movies.append(details.copy())
+
+    return All_movies
+scrapped_movies = scrape_top_list()
+
+def group_by_year(movies):
+    years = []
+    for i in movies:
+        year = i['year']
+        if year not in years:
+            years.append(year)
+    movies_dict_data = {i:[]for i in years}
+    # print(movies_dict_data)
+    for i in movies:
+        year = i['year']
+        for j in movies_dict_data:
+            if str(j) == str(year):
+                # print(movies_dict_data[j])
+                movies_dict_data[j].append(i)
+    return movies_dict_data
+
+    # print(years)
+pprint.pprint(group_by_year(scrapped_movies))
